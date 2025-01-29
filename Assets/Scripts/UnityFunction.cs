66047a83394ee0087501ad5390b4cc6aa2fd2ea6
mathrixyz <cinaryavuz@gmail.com>
2025-01-29 04:03:28 +0300

    1stt

---
+++ Assets/Scripts/UnityFunction.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class UnityFunction : MonoBehaviour
+{
+
+    public GameObject Cube;
+    private void Awake()
+    {
+        Cube.SetActive(false);      
+    }
+
+    //private void OnEnable()
+    //{
+    //    Debug.Log("OnEnable");
+    //}
+
+    //private void OnDisable()
+    //{
+    //    Debug.Log("OnDisable");
+    //}
+
+    //private void Start()
+    //{
+    //    Debug.Log("Start");
+    //}
+
+    //private void Update()
+    //{
+    //    Debug.Log("UPDATE");
+    //}
+
+    //private void FixedUpdate()
+    //{
+    //    Debug.Log("FIXEDUPDATE");
+    //}
+
+    //private void LateUpdate()
+    //{
+    //    Debug.Log("LATEUPDATE");
+    //}
+
+    //private void OnApplicationPause(bool pause)
+    //{
+    //    Debug.Log("PAUSE");
+    //}
+
+    //private void OnApplicationQuit()
+    //{
+    //    Debug.Log("QUIT");
+    //}
+
+
+}
