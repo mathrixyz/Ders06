8b71fb4def9a2193d93df40a5d38f30e02cb7487
mathrixyz <cinaryavuz@gmail.com>
2025-01-29 19:07:30 +0300

    next

--- Assets/Scripts/CarPAthScripts/UI/UIManager.cs
+++ Assets/Scripts/CarPAthScripts/UI/UIManager.cs
@@ -3,13 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-   public void RetryLevelButton()
+    public int level = 0;
+    public int maxLevel = 1;
+
+    public void RetryLevelButton()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);    
     }
 
     public void NextLevelButton()
     {
+        level++;
+        if (level > maxLevel)
+            level = 0;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
 }
